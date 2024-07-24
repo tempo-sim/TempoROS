c5e47a070b4043255a6cda70ebf0d084dd918f91
Peter Melick <165929802+pete-tempo@users.noreply.github.com>
2024-07-24 06:50:29 -0400

    Empty Plugin

---
+++ Source/TempoROS/Public/TempoROS.h
@@ -0,0 +1,15 @@
+﻿// Copyright Tempo Simulation, LLC. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Modules/ModuleManager.h"
+
+TEMPOROS_API DECLARE_LOG_CATEGORY_EXTERN(LogTempoROS, Log, All);
+
+class TEMPOROS_API FTempoROSModule : public IModuleInterface
+{
+public:
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+};
