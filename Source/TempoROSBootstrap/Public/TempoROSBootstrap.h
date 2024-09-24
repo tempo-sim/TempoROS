e0e5e2f85262955b58792bf555f5445d3cc64383
Peter Melick <pete@temposimulation.com>
2024-09-24 18:59:32 -0400

    More Cleanup

--- Source/TempoROSBootstrap/Public/TempoROSBootstrap.h
+++ Source/TempoROSBootstrap/Public/TempoROSBootstrap.h
@@ -5,12 +5,12 @@
 #include "CoreMinimal.h"
 #include "Modules/ModuleManager.h"
 
-// ROS uses runtime dynamic linking of dlls. It won't be able to find those dlls at
-// runtime unless their containing directory is on the user's PATH variable. In the
-// packaged game, adding the TempoROS/Source/ThirdParty/rclcpp/Binaries/Windows directory
-// to the user's PATH is the only solution. However in the Editor, where modules are
-// loaded as individual dlls, we can use this special "bootsttrap" module to effectively
-// add that directory to the PATH for the user *before* TempoROS module is loaded.
+// ROS uses runtime dynamic linking of .dlls. On Windows, since those .dlls live in the Binaries directory, as opposed
+// to the dylibs on Mac and .sos on Linux, which live in the Libraries directory, it won't be able to find them at
+// runtime unless the Binaries directory is on the user's PATH variable. In the packaged game, adding the
+// TempoROS/Source/ThirdParty/rclcpp/Binaries/Windows directory to the user's PATH is the only solution. However in the
+// Editor, where modules are themselves loaded as individual dlls, we can use this special "bootstrap" module to
+// effectively add that directory to the PATH as long as it is initialized *before* the TempoROS module.
 class FTempoROSBootstrapModule : public IModuleInterface
 {
 public:
