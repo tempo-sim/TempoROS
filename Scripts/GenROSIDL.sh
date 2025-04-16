3114c5d40d06f5213ad97158810670c7eedcefe4
Peter Melick <165929802+pete-tempo@users.noreply.github.com>
2025-04-16 15:40:28 -0400

    Make GenROSIDL More Reliable on Windows (#40)

--- Scripts/GenROSIDL.sh
+++ Scripts/GenROSIDL.sh
@@ -25,7 +25,12 @@ if [ -z ${PLATFORM_FOLDER+x} ]; then
   exit 1
 fi
 
-GENTOOL="$RCLCPP_DIR/Binaries/$PLATFORM_FOLDER/rosidl"
+if [[ "$OSTYPE" = "msys" ]]; then
+  GENTOOL="$RCLCPP_DIR/Binaries/$PLATFORM_FOLDER/rosidl-script.py"
+else
+  GENTOOL="$RCLCPP_DIR/Binaries/$PLATFORM_FOLDER/rosidl"
+fi
+
 export PYTHONPATH="$RCLCPP_DIR/Libraries/$PLATFORM_FOLDER/python3.11/site-packages"
 if [[ "$OSTYPE" = "msys" ]]; then
   export AMENT_PREFIX_PATH="$RCLCPP_DIR/Binaries/$PLATFORM_FOLDER"
@@ -160,12 +165,8 @@ GEN_MODULE_MSG_AND_SRVS() {
             GEN_COMMAND+=" -I $(realpath "$SUBDIR")"
         fi
     done
-    if [[ "$OSTYPE" = "msys" ]]; then
-      eval "$GEN_COMMAND" 1> /dev/null
-    else
-      # This is using python from the virtual environment
-      python $GEN_COMMAND 1> /dev/null
-    fi
+    # This is using python from the virtual environment
+    python $GEN_COMMAND 1> /dev/null
   done
 
   cd "$MODULE_GEN_TEMP_DIR"
@@ -296,35 +297,68 @@ GEN_MODULE_MSG_AND_SRVS() {
 
 echo "Generating ROS IDL code..."
 
-if [[ "$OSTYPE" != "msys" ]]; then
-  # Create and activate a virtual environment, or use the main Tempo one,
-  # using Unreal's packaged Python, and install dependencies.
-  VENV_DIR="$PROJECT_ROOT/TempoEnv"
-  if [ ! -d "$VENV_DIR" ]; then
-    "$ENGINE_DIR"/Binaries/ThirdParty/Python3/$PLATFORM_FOLDER/bin/python3 -m venv "$VENV_DIR"
-  fi
-  source "$VENV_DIR/bin/activate"
-  # Suppress pip's warning to upgrade to a new pip. We're using the pip version that came with Unreal, and we want to stay on it.
-  if [[ "$OSTYPE" = "msys" ]]; then
-    echo -e "[global]\ndisable-pip-version-check = true" > "$VENV_DIR/pip.ini"
-  else
-    echo -e "[global]\ndisable-pip-version-check = true" > "$VENV_DIR/pip.conf"
-  fi
-  set +e # Proceed despite errors from pip. That could just mean the user has no internet connection.
-  pip install colcon-common-extensions --quiet --retries 0
-  pip install pyyaml==6.0.2 --quiet --retries 0
-  pip install empy==3.3.4 --quiet --retries 0
-  pip install lark==1.1.1 --quiet --retries 0
-  # 'pip install netifaces' builds from source, but Unreal's python config has a bunch of hard-coded
-  # paths to some engineer's machine, which makes that difficult. So we use this pre-compiled one for
-  # Python3.11 instead.
-  if [[ "$OSTYPE" = "darwin"* ]]; then
-    pip install "$PLUGIN_ROOT/Resources/netifaces/netifaces-0.11.0-cp311-cp311-macosx_10_9_universal2.whl" --quiet --retries 0
-  elif [[ "$OSTYPE" = "linux-gnu"* ]]; then
-    pip install "$PLUGIN_ROOT/Resources/netifaces/netifaces-0.11.0-cp311-cp311-linux_x86_64.whl" --quiet --retries 0
-  fi
-  set -e
+# Using the Python that comes with Unreal
+# Unreal's Python seems to have it's include directory configured incorrectly (`python3-config --include` returns a
+# path to Engine/Binaries/ThirdParty, but the correct include directory is in Engine/Source/ThirdParty).
+# So, we help pip find it, which it may need to in order to build any dependencies from source, with CPATH
+if [[ "$OSTYPE" = "msys" ]]; then
+  PYTHON_DIR="$ENGINE_DIR/Binaries/ThirdParty/Python3/Win64"
+  export CPATH="$CPATH:$UNREAL_ENGINE_PATH/Engine/Source/ThirdParty/Python3/Win64/include"
+elif [[ "$OSTYPE" = "darwin"* ]]; then
+  PYTHON_DIR="$ENGINE_DIR/Binaries/ThirdParty/Python3/Mac/bin"
+  export CPATH="$CPATH:$UNREAL_ENGINE_PATH/Engine/Source/ThirdParty/Python3/Mac/include"
+elif [[ "$OSTYPE" = "linux-gnu"* ]]; then
+  PYTHON_DIR="$ENGINE_DIR/Binaries/ThirdParty/Python3/Linux/bin"
+  export CPATH="$CPATH:$UNREAL_ENGINE_PATH/Engine/Source/ThirdParty/Python3/Linux/include"
 fi
+
+# Create (unless a TempoEnv with the same Python already exists) and activate the virtual environment to generate the API.
+cd "$PYTHON_DIR"
+VENV_DIR="$PROJECT_ROOT/TempoEnv"
+VENV_EXISTS=0
+if [ -f "$VENV_DIR/pyvenv.cfg" ]; then
+  VENV_PYTHON_DIR=$(grep "home = " "$VENV_DIR/pyvenv.cfg" | sed 's/^home = //' | tr '\\' '/')
+  if [[ "$VENV_PYTHON_DIR" = "$PYTHON_DIR" ]]; then
+    VENV_EXISTS=1
+  else
+    rm -rf "$VENV_DIR"
+  fi
+fi
+if [ "$VENV_EXISTS" -eq 0 ]; then
+  if [[ "$OSTYPE" = "msys" ]]; then
+    ./python.exe -m venv "$VENV_DIR"
+  else
+    ./python3 -m venv "$VENV_DIR"
+  fi
+fi
+if [[ "$OSTYPE" = "msys" ]]; then
+  source "$VENV_DIR/Scripts/activate"
+else
+  source "$VENV_DIR/bin/activate"
+fi
+
+# Suppress pip's warning to upgrade to a new pip. We're using the pip version that came with Unreal, and we want to stay on it.
+if [[ "$OSTYPE" = "msys" ]]; then
+  echo -e "[global]\ndisable-pip-version-check = true" > "$VENV_DIR/pip.ini"
+else
+  echo -e "[global]\ndisable-pip-version-check = true" > "$VENV_DIR/pip.conf"
+fi
+set +e # Proceed despite errors from pip. That could just mean the user has no internet connection.
+pip install colcon-common-extensions --quiet --retries 0
+pip install pyyaml==6.0.2 --quiet --retries 0
+pip install empy==3.3.4 --quiet --retries 0
+pip install lark==1.1.1 --quiet --retries 0
+# 'pip install netifaces' builds from source, but Unreal's python config has a bunch of hard-coded
+# paths to some engineer's machine, which makes that difficult. So we use this pre-compiled one for
+# Python3.11 instead.
+if [[ "$OSTYPE" = "darwin"* ]]; then
+  pip install "$PLUGIN_ROOT/Resources/netifaces/netifaces-0.11.0-cp311-cp311-macosx_10_9_universal2.whl" --quiet --retries 0
+elif [[ "$OSTYPE" = "linux-gnu"* ]]; then
+  pip install "$PLUGIN_ROOT/Resources/netifaces/netifaces-0.11.0-cp311-cp311-linux_x86_64.whl" --quiet --retries 0
+elif [[ "$OSTYPE" = "msys" ]]; then
+  pip install "$PLUGIN_ROOT/Resources/netifaces/netifaces-0.11.0-cp311-cp311-win_amd64.whl" --quiet --retries 0
+fi
+set -e
 
 # Find dotnet
 cd "$ENGINE_DIR"
